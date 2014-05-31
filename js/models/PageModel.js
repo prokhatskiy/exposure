ab93ab739daca792ca383026d96dbb8a895c2358
Artem Prokhatskiy <prokhatskiy@gmail.com>
2014-05-31 22:42:52 +0300

    added prev next links

--- js/models/PageModel.js
+++ js/models/PageModel.js
@@ -1,6 +1,8 @@
 define(['backbone'], function(Backbone) {
 	return PageModel = Backbone.Model.extend({
 		'headImgSrc' : '',
-		'content' : ''
+		'content' : '',
+		'nextLink' : '',
+		'prevLink' : ''
 	});       
 });
